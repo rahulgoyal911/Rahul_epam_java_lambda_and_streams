8a444c4928aef5f2692a3b13751d8565cdd97c09
rahulgoyal911 <rahulgoyal0.rg@gmail.com>
2020-06-22 23:13:39 +0530

    task completed

---
+++ src/main/java/org/example/Ques2.java
@@ -0,0 +1,19 @@
+package org.example;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Ques2 {
+    public static void main(String args[]) {
+        List<String> l1 = new ArrayList<String>();
+        l1.add("aba");
+        l1.add("abb");
+        l1.add("addddda");
+        l1=search(l1);
+        for(String i:l1)
+            System.out.println(i);
+    }
+    static List<String> search(List<String> l1)
+    {
+        return l1.stream().filter(s -> s.startsWith("a")).filter(s -> s.length()==3).collect(Collectors.toList());
+    }
+}
